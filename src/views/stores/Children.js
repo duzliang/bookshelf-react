e6a8a1849979576592d57fcc124f23a46a3513dc
Duke <duzliang@gmail.com>
2022-12-27 20:59:41 +0800

    refactor: refactor connect()

--- src/views/stores/Children.js
+++ src/views/stores/Children.js
@@ -5,12 +5,28 @@ class Children extends React.Component {
   render() {
     return (
       <div>
-        {this.props.getState().count}
-        <button onClick={() => this.props.dispatch({ type: 'AddOne' })}>add 1</button>
-        <button onClick={() => this.props.dispatch({ type: 'MultiThree' })}>multi 3</button>
+        {this.props.count}
+        <button onClick={() => this.props.AddOne()}>add 1</button>
+        <button onClick={() => this.props.MultiThree()}>multi 3</button>
       </div>
     )
   }
 }
 
-export default connect(Children);
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    AddOne: () => {
+      dispatch({ type: 'AddOne' })
+    },
+    MultiThree: () => {
+      dispatch({ type: 'MultiThree' })
+    }
+  }
+}
+
+export default connect(mapStateToProps)(mapDispatchToProps)(Children);
--- src/views/stores/connect.js
+++ src/views/stores/connect.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { ThemeContext } from "./context";
 
-const connect = (Component) => (
-  class WrapperComponent extends React.Component {
+const connect = (mapStateToProps) => (mapDispatchToProps) => (Component) => (
+  class ChildComponent extends React.Component {
     render() {
       return (
         <ThemeContext.Consumer>
-          {context => <Component {...context.store} />}
+          {context => {
+            const stateProps = mapStateToProps ? mapStateToProps(context.store.getState()) : {};
+            const dispatchProps = mapDispatchToProps ? mapDispatchToProps(context.store.dispatch) : {};
+            const allProps = {
+              ...stateProps,
+              ...dispatchProps,
+              ...this.props,
+            };
+
+            return <Component {...allProps} />
+          }}
         </ThemeContext.Consumer>
       )
     }
